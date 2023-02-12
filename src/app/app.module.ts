802c530679fd5c14a6cc9183db2b9ea065dc11f8
Sujane Lopez <sujane.lopez@clenergy-ev.com>
2023-02-12 15:17:57 +0000

    Added the token to authentication, attached to the requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostService } from './posts/post.service';
-import {HttpClientModule} from '@angular/common/http/'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http/'
 import { AppRoutingModule } from './app-routing.module';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInterceptor } from './auth/auth-interceptor';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,8 @@ import { SignupComponent } from './auth/signup/signup.component';
     MatPaginatorModule,
     FormsModule
   ],
-  providers:  [PostService],
+  providers:  [{provide: HTTP_INTERCEPTORS, useClass:AuthInterceptor, multi:true},
+    PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -8,8 +8,12 @@ import { AuthData } from "./auth-data.model";
 @Injectable({ providedIn: 'root' })
 
 export class AuthService {
+  private token!: string;
   constructor(private http: HttpClient, private router: Router) { }
 
+   getToken(){
+    return this.token;
+   }
 
   createUser(email:string, password:string) {
     let authData: AuthData = {email: email, password:password}
@@ -21,9 +25,10 @@ export class AuthService {
 
   login(email:string, password:string){
     let authData: AuthData = {email: email, password:password}
-    this.http.post<AuthData>("http://localhost:3000/api/user/login", authData)
+    this.http.post<{token:string}>("http://localhost:3000/api/user/login", authData)
     .subscribe(response => {
-        console.log(response)
+        const token = response.token;
+        this.token = token;
     })
   }
 }
